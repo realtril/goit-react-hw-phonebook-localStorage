e20ab6c22e92dfe1ef53a5e95e545bb371a54700
Oleksandr Tril <tril.oleksandr@gmail.com>
2020-07-22 18:53:04 +0300

    Updates

---
+++ precache-manifest.e7d68f358c77972d98f835a953446666.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c690ab1538db133a88a1f4b62983b41e",
+    "url": "/goit-react-hw-phonebook-localStorage/index.html"
+  },
+  {
+    "revision": "d31fdcc5c7d96d541ec9",
+    "url": "/goit-react-hw-phonebook-localStorage/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "498ac61e04b7a1fbbe66",
+    "url": "/goit-react-hw-phonebook-localStorage/static/js/2.96bf3192.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-phonebook-localStorage/static/js/2.96bf3192.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d31fdcc5c7d96d541ec9",
+    "url": "/goit-react-hw-phonebook-localStorage/static/js/main.08738313.chunk.js"
+  },
+  {
+    "revision": "17afc900f5340b5dafbb",
+    "url": "/goit-react-hw-phonebook-localStorage/static/js/runtime-main.60e151fd.js"
+  }
+]);
